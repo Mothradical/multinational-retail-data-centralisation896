be0ac311e55b4ca27871e1d3b8b16844e1ede2ff
El Shute <elshute2@gmail.com>
2024-11-07 07:54:55 +0000

    Class methods completed as functions, not yet fully functional as Class methods

--- data_extraction.py
+++ data_extraction.py
@@ -2,7 +2,10 @@ import yaml
 from sqlalchemy import create_engine
 from sqlalchemy import inspect
 import pandas as pd
+import requests
 import tabula
+import ast
+import boto3
 pdf_path = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
 
 
@@ -11,11 +14,33 @@ class DataExtractor:
     def __init__(self, DatabaseConnector):
         self.DatabaseConnector = DatabaseConnector
         
-    def read_rds_tables(self, DatabaseConnector):
+    def read_rds_tables(self, DatabaseConnector, table_name):
         engine = DatabaseConnector.init_db_engine()
-        users_df = pd.read_sql_table('legacy_users', engine)
+        users_df = pd.read_sql_table(table_name, engine)
         return users_df
     
     def retrieve_pdf_data(self, pdf_path):
-        df = tabula.read_pdf(pdf_path, stream=True)
+        dfs = tabula.read_pdf(pdf_path, multiple_tables=True, stream=True, lattice=True, pages='all')
+        df = pd.concat(dfs)
         return df
+
+    def list_number_of_stores(self, endpoint, headers_dict):
+        response = requests.get(endpoint, headers=headers_dict)
+        return response
+
+    def retrieve_stores_data(self):
+        x = list(range(0, 451))
+        list_of_dicts = []
+        for n in x:
+            url = f"https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{n}"
+            headers = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
+            response = requests.get(url, headers=headers)
+            data = response.text
+            data_dict = ast.literal_eval(data.replace("null", "'N/A'"))
+            list_of_dicts.append(data_dict)
+        data_df = pd.DataFrame(list_of_dicts)
+        return data_df
+    
+    def extract_from_s3(self, url):
+        data = pd.read_csv(url)
+        return data
--- database_utils.py
+++ database_utils.py
@@ -7,9 +7,8 @@ import psycopg2
 
 class DatabaseConnector:
 
-    def __init__(self, df, table_name):
-        self.df = df
-        self.table_name = table_name
+    def __init__(self):
+        print("init")
 
     def read_db_creds(self):
         with open('C:/Users/elshu/OneDrive/Documents/Data/AiCore_Projects/multinational-retail-data-centralisation896/db_creds.yaml') as creds:
@@ -28,16 +27,8 @@ class DatabaseConnector:
         return list_of_tables
 
     def upload_to_db(self, df, table_name):
-        engine = create_engine('postgresql://postgres:password@localhost:5432/sales_data')
-        df.to_sql(table_name, engine)
+        self.engine = create_engine('postgresql://postgres:password@localhost:5432/sales_data')
+        df.to_sql(table_name, self.engine)
 
-## NEXT TIME! TRY AND USE THIS ABOVE METHOD TO ADD DATA TO PGADMIN
-
-
-#        HOST = 'local_host'
-#        USER = 'postgres'
-#        PASSWORD = 'password'
-#        DATABASE = 'sales_data'
-#        PORT = 5432
-#        with psycopg2.connect(host=HOST, user=USER, password=PASSWORD, database=DATABASE, port=PORT) as conn:
-#            with conn.cursor() as cur:
+database = DatabaseConnector()
+print(database.list_db_tables())
