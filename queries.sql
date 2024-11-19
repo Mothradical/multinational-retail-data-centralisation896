50348cdeef74a514055dc1a4c6f10b0d73d41094
Mothradical <elshute2@gmail.com>
2024-11-19 10:07:03 +0000

    Update queries.sql
    Removed extra " -- "

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
--- Task 1: How many stores does the business have and in which countries? --
+-- Task 1: How many stores does the business have and in which countries?
 
 SELECT
     country_code AS country,
